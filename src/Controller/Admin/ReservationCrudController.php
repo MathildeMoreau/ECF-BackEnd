a360f9cacefb1af5a19e675a2b9d881c3cfb7c4a
Mathilde Moreau <mathilde.moreau85@gmail.com>
2023-05-23 21:15:48 +0200

    Changement champs Easyadmin Reservation

--- src/Controller/Admin/ReservationCrudController.php
+++ src/Controller/Admin/ReservationCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Reservation;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
@@ -23,10 +24,13 @@ class ReservationCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            AssociationField::new('client'),
             NumberField::new('nb_couverts', 'Nombre de couverts'),
-            DateField::new('date'),
-            TimeField::new('heure')
+            TextField::new('date'),
+            TimeField::new('heure'),
+            TextField::new('nom'),
+            EmailField::new('email'),
+            TextField::new('allergies')
+
         ];
     }
 }
